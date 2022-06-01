5a89236d4eed6932895f9e5983a5bfac6a08c86a
Colin Harfst <colinharfst@gmail.com>
2022-06-01 15:49:04 -0700

    Update test file

--- src/Home.test.js
+++ src/Home.test.js
@@ -1,8 +1,6 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import Home from "./Home";
 
-test("renders learn react link", () => {
-	render(<App />);
-	const linkElement = screen.getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
+test("Renders <Home />", () => {
+	render(<Home />);
 });
